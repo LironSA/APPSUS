0f70a3229a89ff283c756e3e766e1cefa2f336a4
Liron Shahar-Atia <liron.shaharatia@gmail.com>
2019-11-06 15:46:02 +0200

    AAA

--- js/pages/email/cmps/new-mail.cmp.js
+++ js/pages/email/cmps/new-mail.cmp.js
@@ -2,13 +2,31 @@
 
 export default {
     template: `
-                <section class="new-mail">
-                    <h1>new mail section</h1>
+                <section class="new-email">
+                    <h1 class="new-email-header">New Message</h1>
+                    <h2>To: <input type="email" class="send-to" placeholder="Email Address" v-model="emailData.sentTo"/></h2>
+                    <h2>Cc: <input type="email" class="send-to" placeholder="Email Address" v-model="emailData.sentToCc"/></h2>
+                    <h2>Bcc: <input type="email" class="send-to" placeholder="Email Address" v-model="emailData.sentToBcc"/></h2>
+                    <h2>Subject: <input type="text" class="send-to" v-model="emailData.subject"/></h2>
+                    <textarea v-model="emailData.body"></textarea>
+
+                    <section>
+                        <button @click="sendEmail">Send</button>
+                        <button @click="draftEmail">Draft</button>
+                        <button @click="deleteEmail">Delete</button>
+
+                    </section>            
                 </section>            
     `,
     data(){
         return{
-
+            emailData: {
+                sentTo: '',
+                sentToCc: '',
+                sentToBcc: '',
+                subject: '',
+                body: '',
+            }
         }
     },
     created(){
