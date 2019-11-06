faaf54013832613f092ec93fa0710e22e3da0b31
Inon Amsalem <inon049@gmail.com>
2019-11-06 11:40:34 +0200

    fs\

--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -6,13 +6,14 @@ export default {
     <header class="app-header">
         <h1> im the header </h1>
         <nav>
-            <router-link>Home Page</router-link>
-            <router-link>Email</router-link>
-            <router-link>Notes</router-link>
+            <router-link to="/home">Home Page</router-link>
+
         </nav>
     </header>
     
     `,
+                // <router-link>Email</router-link>
+            // <router-link>Notes</router-link>/
     components: {
         userMsg
     }
