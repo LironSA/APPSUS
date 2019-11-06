798e3b5b950507321cc3ed0eb32197f78a04ff4e
Liron Shahar-Atia <liron.shaharatia@gmail.com>
2019-11-06 12:42:33 +0200

    Starting

--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -3,17 +3,18 @@ import userMsg from './user-msg.cmp.js'
 
 export default {
     template: `
-    <header class="app-header">
-        <h1> im the header </h1>
-        <nav>
-            <router-link to="/home">Home Page</router-link>
+    <header class="app-header flex space-between">
+        <h1 class="header-logo"> APPSUS </h1>
+        <nav class="nav-header flex space-around">
+            <router-link to="/home">Home Page</router-link> 
+            <!-- <router-link>Email</router-link> -->
+            <!-- <router-link>Notes</router-link>/ -->
+            <router-link to="/about">About</router-link>
 
         </nav>
     </header>
     
     `,
-                // <router-link>Email</router-link>
-            // <router-link>Notes</router-link>/
     components: {
         userMsg
     }
