787f5fe55edfc52b5ddc5360a4c283f4ba8775fe
Inon Amsalem <inon049@gmail.com>
2019-11-06 11:43:30 +0200

    ready to roll

--- js/main.js
+++ js/main.js
@@ -8,7 +8,6 @@ let app = {
     el: '.my-app',
     template:
         `   <div>
-                <user-msg></user-msg>
                 <app-header></app-header>
                 <router-view></router-view>
             </div>
