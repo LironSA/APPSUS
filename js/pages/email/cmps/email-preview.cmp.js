3482e9cf5e5d56ed846661784362e5a8466eef04
Liron Shahar-Atia <liron.shaharatia@gmail.com>
2019-11-06 18:24:02 +0200

    1

--- js/pages/email/cmps/email-preview.cmp.js
+++ js/pages/email/cmps/email-preview.cmp.js
@@ -2,14 +2,17 @@
 export default {
     props: ['email', 'isSelected'],
     template: `
-        <li>
-            <h4>{{email.receivedFrom.name}}</h4>
-            <h4>{{email.subject}}</h4>
-            <h4>{{email.body}}</h4>
-            <h4>{{email.sentAt}}</h4>
-        </li>
+        <section v-else-if="isSelected" >
+        
+            <li v-if="email">
+                <h4>{{email.receivedFrom.name}}</h4>
+                <h4>{{email.subject}}</h4>
+                <h4>{{email.body}}</h4>
+                <h4>{{email.sentAt}}</h4>
+            </li>
+        </section>
        
-        <section v-if="isSelected">
+        <section v-else-if="isSelected" >
             <li class="selected-preview">
                 <h2>{{email.subject}}</h2>
                 <h4>{{email.receivedFrom.name}}</h4>
