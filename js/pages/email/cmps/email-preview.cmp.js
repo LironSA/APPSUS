4dd3c35d2599b8f439622145137f5c336ade9da5
Liron Shahar-Atia <liron.shaharatia@gmail.com>
2019-11-06 20:20:28 +0200

    end day 1

--- js/pages/email/cmps/email-preview.cmp.js
+++ js/pages/email/cmps/email-preview.cmp.js
@@ -3,26 +3,28 @@ export default {
     props: ['email', 'isSelected'],
     template: `
         <section class="preview-container" v-if="email" >
-            <section class="preview-list" >
-                <li >
-                    <h4>aaa</h4>
-                    <!-- <h4>{{email.subject}}</h4>
-                    <h4>{{email.body}}</h4>
-                    <h4>{{email.sentAt}}</h4> -->
+                <li class="flex">
+                    <h4 class="prev-name">{{email.receivedFrom.name}}</h4>
+                    <h4 class="prev-subj">{{email.subject}} - 
+                        <a>{{email.body}}</a></h4>
+                    <h4 class="prev-sent">{{email.sentAt}}</h4>
                 </li>
-        </section>
-<!--        
-        <section v-if="isSelected" >
+       
+        <!-- <section v-if="isSelected" >
             <li class="selected-preview">
-                <h2>bbb</h2>
-                <!-- <h4>{{email.receivedFrom.name}}</h4>
+                <h2>{{email.subject}}</h2>
+                <h4>{{email.receivedFrom.name}}</h4>
                 <h4>{{email.receivedFrom.addr}}</h4>
-                <h4>{{email.body}}</h4> -->
+                <h4>{{email.body}}</h4>
             </li>
         </section> -->
     </section>
     `,
-    created: {
-        
-    }
+    // TODO - use MOMENT 
+    // computed: {
+    //     hours() {
+    //         return this.email.sentAt.toLocaleDateString();
+    //         }
+    //     },
+      
 }
