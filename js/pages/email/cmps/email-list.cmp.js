00b74ba7b25cd1d7b5e2fe1ac43bba70e04d5bbd
Liron Shahar-Atia <liron.shaharatia@gmail.com>
2019-11-06 17:23:45 +0200

    preview

---
+++ js/pages/email/cmps/email-list.cmp.js
@@ -0,0 +1,23 @@
+import emailPreview from  './email-preview.cmp.js';
+
+export default {
+    props: ['emails'],
+    template: `
+    <section>
+        <ul class="email-list">
+            <email-preview v-for="email in emails" :key="email.id" v-bind:email="email">
+            </email-preview>
+        </ul>
+
+    </section>
+    `,
+    data() {
+        return {
+        selectedIds: [],
+        }
+    },
+    components: {
+        emailPreview
+    }
+}
+
