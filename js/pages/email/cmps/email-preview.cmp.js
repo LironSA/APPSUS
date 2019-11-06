af1c97a397d315a150ebedf1d6baf6df00e04feb
Inon Amsalem <inon049@gmail.com>
2019-11-06 20:21:38 +0200

    20:00 merge

--- js/pages/email/cmps/email-preview.cmp.js
+++ js/pages/email/cmps/email-preview.cmp.js
@@ -1,27 +1,29 @@
 
 export default {
-    props: ['email', 'isSelected'],
+    props: ['email'],
     template: `
-        <section v-else-if="isSelected" >
-        
+        <section>
             <li v-if="email">
                 <h4>{{email.receivedFrom.name}}</h4>
                 <h4>{{email.subject}}</h4>
                 <h4>{{email.body}}</h4>
                 <h4>{{email.sentAt}}</h4>
             </li>
+            <section v-if="isSelected" >
+                <li class="selected-preview">
+                    <h2>{{email.subject}}</h2>
+                    <h4>{{email.receivedFrom.name}}</h4>
+                    <h4>{{email.receivedFrom.addr}}</h4>
+                    <h4>{{email.body}}</h4>
+                </li>
+            </section>
         </section>
        
-        <section v-else-if="isSelected" >
-            <li class="selected-preview">
-                <h2>{{email.subject}}</h2>
-                <h4>{{email.receivedFrom.name}}</h4>
-                <h4>{{email.receivedFrom.addr}}</h4>
-                <h4>{{email.body}}</h4>
-            </li>
-        </section>
-    `,
-    created: {
         
+    `,
+    data(){
+        return{
+            isSelected:false,
+        }
     }
 }
--- js/pages/email/email.cmp.js
+++ js/pages/email/email.cmp.js
@@ -13,29 +13,47 @@ export default {
     components: {
         emailMenu
     },
-    data(){
-        return{
-            emails:[]
+    data() {
+        return {
+            emails: [],
+            filterBy: {
+                type: '',
+                selectVal: '',
+                txtSearch: ''
+            }
         }
     },
     created() {
         emailService.getEmails()
             .then(emails => {
                 console.log(emails);
-                this.emails=emails
+                this.emails = emails
             })
     },
-    computed: {
-        filteredEmails() {
-            if (this.$route.name === 'a') { return { foo: this.foo } }
-            if (this.$route.name === 'b') { return { bar: this.bar } }
+    methods: {
+        filterEmails() {
+            let filteredEmails;
+            this.emails.forEach(email => {
+                if (email.type) filteredEmails.push
+            })
 
         }
     },
-    watch:{
-        '$route.params.type'(){
-            console.log('gotem',this.$route.params.type);
-        } 
+    computed: {
+        filteredEmails() {
+            if (filterBy.type === 'starred') {
+
+                return
+            }
+            // if (filterBy.type === 'starred')
+            //     if (filterBy.type === 'starred')
+            //         if (filterBy.type === 'starred')
+        }
+    },
+    watch: {
+        '$route.params.type'() {
+            console.log('gotem', this.$route.params.type);
+        }
     }
 
 }
