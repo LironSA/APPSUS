b036974da05197490ceb57530d74ef6d8548026c
Liron Shahar-Atia <liron.shaharatia@gmail.com>
2019-11-06 13:31:29 +0200

    Nav-flex

--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -1,19 +1,37 @@
 import userMsg from './user-msg.cmp.js'
 
 
+
 export default {
     template: `
     <header class="app-header flex space-between">
         <h1 class="header-logo"> APPSUS </h1>
-        <nav class="nav-header flex space-around">
-            <router-link to="/home">Home Page</router-link> 
-            <!-- <router-link>Email</router-link> -->
-            <!-- <router-link>Notes</router-link>/ -->
+        <button @click="toggleMenu">#</button>
+        <nav class="nav-header flex wrap" v-if="isOpenMenu">
+            <router-link to="/email"><img src="img/email-origin.JPG" alt=""> </router-link> 
+            <router-link to="/notes"><img src="img/notes_origin.JPG" alt=""> </router-link> 
+            <router-link to="/maps"><img src="img/maps.JPG" alt=""> </router-link> 
+            <router-link to="/calander"><img src="img/calander.JPG" alt=""> </router-link> 
+            <router-link to="/youtube"><img src="img/youtube.JPG" alt=""> </router-link> 
+            <router-link to="/contacts"><img src="img/contacts.JPG" alt=""> </router-link> 
+            <router-link to="/photos"><img src="img/photos.JPG" alt=""> </router-link> 
+            <router-link to="/earth"><img src="img/earth.JPG" alt=""> </router-link> 
+            <router-link to="/news"><img src="img/news.JPG" alt=""> </router-link> 
             <router-link to="/about">About</router-link>
         </nav>
     </header>
     
     `,
+    data() {
+        return {
+            isOpenMenu: false
+        }
+    },
+    methods: {
+        toggleMenu() {
+            this.isOpenMenu = !this.isOpenMenu
+        }
+    },
     components: {
         userMsg
     }
