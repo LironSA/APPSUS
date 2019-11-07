f693148e5d1504a70e791ab125a0e63cc6050d57
Inon Amsalem <inon049@gmail.com>
2019-11-07 11:35:54 +0200

    asd

---
+++ js/pages/email/cmps/email-filter.cmp.js
@@ -0,0 +1,11 @@
+'use strict';
+
+export default {
+    template: `
+                <section class="email-filter">
+                <input type="text" placeholder="search email">    
+                <select></select>
+                </section>
+
+    `
+}
