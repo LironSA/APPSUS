dcaefc43eb2cc1a7198deadce157dfecb30f4b83
Inon Amsalem <inon049@gmail.com>
2019-11-07 14:32:09 +0200

    111

--- js/pages/email/cmps/email-service.js
+++ js/pages/email/cmps/email-service.js
@@ -37,16 +37,16 @@ function removeEmail(id) {
 
 
 const DEFUALT_EMAILS = [
-    { id: makeId(), sentTo: { to: [], cc: [], bcc: [] }, receivedFrom: { name: 'Jack', addr: 'Jack@gmail.com' }, subject: 'Wassap with Vue?', body: 'May I',isDraft:true, isRead: false, sentAt: 1554580930594 },
-    { id: makeId(), sentTo: { to: [], cc: [], bcc: [] }, receivedFrom: { name: 'Stan', addr: 'Stan@gmail.com' }, subject: 'My plans for the summer', body: 'Staying at home its too hot outside', isRead: false, sentAt: 155111111594 },
-    { id: makeId(), sentTo: { to: [], cc: [], bcc: [] }, receivedFrom: { name: 'Roy', addr: 'Roy@gmail.com' }, subject: 'Sprint progress status ', body: 'just faking data',isStarred:true, isRead: false, sentAt: 151569930594 },
-    { id: makeId(), sentTo: { to: [], cc: [], bcc: [] }, receivedFrom: { name: 'Gus', addr: 'Gus@gmail.com' }, subject: 'God damn', body: 'This is hard', isRead: false, sentAt: 1551133458594 },
-    { id: makeId(), sentTo: { to: [], cc: [], bcc: [] }, receivedFrom: { name: 'Lee', addr: 'Lee@gmail.com' }, subject: 'Blabla', body: 'Blyat', isRead: true, sentAt: 155715930594 },
-    { id: makeId(), sentTo: { to: [], cc: [], bcc: [] }, receivedFrom: { name: 'John Doe', addr: 'John@gmail.com' }, subject: 'Haha', body: 'hhhhhh', isRead: true, sentAt: 155113393999 },
-    { id: makeId(), sentTo: { to: [], cc: [], bcc: [] }, receivedFrom: { name: 'Eleanor Rigby', addr: 'Eleanor@gmail.com' }, subject: 'Gotem', body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis, corporis!', isRead: false,isSent:true, sentAt: 1361133930594 },
-    { id: makeId(), sentTo: { to: [], cc: [], bcc: [] }, receivedFrom: { name: 'Roxann', addr: 'Roxann@gmail.com' }, subject: 'Lorem', body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis, corporis!', isRead: false,isDraft:true, sentAt: 1231133930594 },
-    { id: makeId(), sentTo: { to: [], cc: [], bcc: [] }, receivedFrom: { name: 'My Sharona', addr: 'Sharona@gmail.com' }, subject: 'Lorem', body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis, corporis!', isRead: false, sentAt: 1551542930594 },
-    { id: makeId(), sentTo: { to: [], cc: [], bcc: [] }, receivedFrom: { name: 'Luka', addr: 'Luka@gmail.com' }, subject: 'Lorem', body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis, corporis!', isRead: false, sentAt: 1559876930594 },
+    { id: makeId(), sentTo: { to: [], cc: [], bcc: [] }, receivedFrom: { name: 'Jack', addr: 'Jack@gmail.com' }, subject: 'Wassap with Vue?', body: 'May I',isDraft:true,isSent:true, isRead: false, sentAt: 1554580930594 },
+    { id: makeId(), sentTo: { to: [], cc: [], bcc: [] }, receivedFrom: { name: 'Stan', addr: 'Stan@gmail.com' }, subject: 'My plans for the summer', body: 'Staying at home its too hot outside',isSent:false, isRead: false, sentAt: 155111111594 },
+    { id: makeId(), sentTo: { to: [], cc: [], bcc: [] }, receivedFrom: { name: 'Roy', addr: 'Roy@gmail.com' }, subject: 'Sprint progress status ', body: 'just faking data',isStarred:true,isSent:false, isRead: false, sentAt: 151569930594 },
+    { id: makeId(), sentTo: { to: [], cc: [], bcc: [] }, receivedFrom: { name: 'Gus', addr: 'Gus@gmail.com' }, subject: 'God damn', body: 'This is hard',isSent:false, isRead: false, sentAt: 1551133458594 },
+    { id: makeId(), sentTo: { to: [], cc: [], bcc: [] }, receivedFrom: { name: 'Lee', addr: 'Lee@gmail.com' }, subject: 'Blabla', body: 'Blyat',isSent:false, isRead: true, sentAt: 155715930594 },
+    { id: makeId(), sentTo: { to: [], cc: [], bcc: [] }, receivedFrom: { name: 'John Doe', addr: 'John@gmail.com' }, subject: 'Haha', body: 'hhhhhh',isSent:false, isRead: true, sentAt: 155113393999  },
+    { id: makeId(), sentTo: { to: [], cc: [], bcc: [] }, receivedFrom: { name: 'Eleanor Rigby', addr: 'Eleanor@gmail.com' }, subject: 'Gotem', body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis, corporis!', isRead: false,isSent:false, sentAt: 1361133930594 },
+    { id: makeId(), sentTo: { to: [], cc: [], bcc: [] }, receivedFrom: { name: 'Roxann', addr: 'Roxann@gmail.com' }, subject: 'Lorem', body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis, corporis!', isRead: false,isDraft:true,isSent:false, sentAt: 1231133930594 },
+    { id: makeId(), sentTo: { to: [], cc: [], bcc: [] }, receivedFrom: { name: 'My Sharona', addr: 'Sharona@gmail.com' }, subject: 'Lorem', body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis, corporis!', isRead: false,isSent:false, sentAt: 1551542930594 },
+    { id: makeId(), sentTo: { to: [], cc: [], bcc: [] }, receivedFrom: { name: 'Luka', addr: 'Luka@gmail.com' }, subject: 'Lorem', body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis, corporis!', isRead: false,isSent:false, sentAt: 1559876930594 },
 ]
 
 getEmails();
