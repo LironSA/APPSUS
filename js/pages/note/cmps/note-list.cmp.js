6af7e92cd4f7e90fc4b2cda2caa8c3c922384c72
Inon Amsalem <inon049@gmail.com>
2019-11-07 16:39:08 +0200

    oip

---
+++ js/pages/note/cmps/note-list.cmp.js
@@ -0,0 +1,10 @@
+'use strict';
+
+export default {
+    props: ['notes'],
+    template: `
+                <section class="note-list-container">
+                    <h1>This is Ze NOTE LIST!</h1>
+                </section>
+    `
+}
--- js/pages/note/keep.cmp.js
+++ js/pages/note/keep.cmp.js
@@ -1,10 +1,28 @@
 'use strict';
-
+import { noteService } from './note-service.js'
+import addNote from './cmps/add-note.cmp.js'
+import noteList from './cmps/note-list.cmp.js'
 export default {
     template: `
             <section class="notes-app-container">
             <add-note></add-note>
-            <note-list></note-list>
+            <note-list v-if="notes":notes="notes"></note-list>
             </section>
             `,
+    components: {
+        addNote,
+        noteList
+    },
+    data() {
+        return {
+            notes: []
+        }
+    },
+    created() {
+        noteService.getNotes()
+            .then(notes => {
+                console.log('notes from service', notes);
+                this.notes = notes
+            })
+    }
 }
