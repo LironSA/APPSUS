ec56370649aef8e92d5482bcbd99cf20a2c9fa9c
Inon Amsalem <inon049@gmail.com>
2019-11-07 16:49:31 +0200

    rgtfdg

--- js/pages/note/note-service.js
+++ js/pages/note/note-service.js
@@ -17,7 +17,7 @@ function getNotes() {
         notes = DEFUALT_NOTES
         storageService.store(NOTES_KEY, notes)
         gNotes = notes;
-    }else{
+    } else {
         gNotes = notes;
     }
     return Promise.resolve(gNotes)
@@ -25,20 +25,20 @@ function getNotes() {
 
 function removeNotes(id) {
     var idx = gNotes.findIndex(note => note.id === id);
-    if (idx !== -1){
-       let note= gNotes.splice(idx, 1)
-       console.log('note:',note);
-    }  
+    if (idx !== -1) {
+        let note = gNotes.splice(idx, 1)
+        console.log('note:', note);
+    }
     storageService.store(NOTES_KEY, gNotes)
     return Promise.resolve();
 }
 
 
 const DEFUALT_NOTES = [
-    { id: makeId(), type: 'p', content:'Lorem ipsum, dolor sit amet, consectetur, adipisicing elit. Facilis, corporis!', isPinned: true, style: {color, bgc, font, 'font-size':'10px' }, createdAt: 1554580930594 },
-    { id: makeId(), type: 'ul', content:'Lorem ipsum, dolor sit amet, consectetur adipisicing, elit. Facilis, corporis!', isPinned: true, style: {color, bgc, font, 'font-size':'10px' }, createdAt: 1664580930594 },
-    { id: makeId(), type: 'p', content:'onsectetur adipisicing elit. Facilis, corporis!', isPinned: true, style: {color, bgc, font, 'font-size':'10px' }, createdAt: 1554580930594 },
-    { id: makeId(), type: 'ul', content:'r sit, amet consectetur, adipisicing elit. Facilis, corporis!', isPinned: true, style: {color, bgc, font, 'font-size':'10px' }, createdAt: 1664580930594 },
-   ]
+    { id: makeId(), type: 'p', content: 'Lorem ipsum, dolor sit amet, consectetur, adipisicing elit. Facilis, corporis!', isPinned: true,  style: { color: '#fff', 'background-color': '#22546', 'font-size': '10px' }, createdAt: 1554580930594 },
+    { id: makeId(), type: 'ul', content: 'Lorem ipsum, dolor sit amet, consectetur adipisicing, elit. Facilis, corporis!', isPinned: true,  style: { color: '#fff', 'background-color': '#22546', 'font-size': '10px' }, createdAt: 1664580930594 },
+    { id: makeId(), type: 'p', content: 'onsectetur adipisicing elit. Facilis, corporis!', isPinned: true, style: { color: '#fff', 'background-color': '#22546', 'font-size': '10px' }, createdAt: 1554580930594 },
+    { id: makeId(), type: 'ul', content: 'r sit, amet consectetur, adipisicing elit. Facilis, corporis!', isPinned: true,  style: { color: '#fff', 'background-color': '#22546', 'font-size': '10px' }, createdAt: 1664580930594 },
+]
 
-getEmails();
+getNotes()
