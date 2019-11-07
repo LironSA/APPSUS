ea470f97d13f7177a4c5abca2747c4fa8318fe3c
Inon Amsalem <inon049@gmail.com>
2019-11-07 18:23:30 +0200

    note-app structure v1

--- js/pages/note/cmps/note-list.cmp.js
+++ js/pages/note/cmps/note-list.cmp.js
@@ -1,10 +1,11 @@
 'use strict';
-
+import txtNotePrv from '../cmps/txt-note-prv.cmp.js'
+import todoNotePrv from '../cmps/todo-note-prv.cmp.js'
 export default {
     props: ['notes'],
     template: `
-                <section class="note-list-container">
-                    <h1>This is Ze NOTE LIST!</h1>
-                </section>
+                <ul class="note-list-container" style ="border: 1px solid black; display:flex; flex-wrap:wrap;"
+                   <component v-for="note in notes" :key="note.id" :is='note.type' :note="note" style ="width:33.33%"></component>
+                </ul>
     `
 }
