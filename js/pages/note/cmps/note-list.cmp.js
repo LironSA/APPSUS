6fd7d7f105e21f51c9796b75f9f30515578eba0f
Inon Amsalem <inon049@gmail.com>
2019-11-10 00:32:56 +0200

    951

--- js/pages/note/cmps/note-list.cmp.js
+++ js/pages/note/cmps/note-list.cmp.js
@@ -5,13 +5,15 @@ import imgNotePrv from '../cmps/img-note-prv.cmp.js'
 import vidNotePrv from '../cmps/vid-note-prv.cmp.js'
 
 export default {
-  props: ['notes'],
+  props: ['notes','title'],
   template: `
-     <ul class="note-list-container clean-list flex wrap">
-          <component v-for="note in notes" :key="note.id" :is='note.type' :note="note" style="position:relative; border:0.3px solid gray; width:30%;" >
-          </component>
-                   
+  <div class="note-list-container">
+  <h4 v-if="title">{{title}}</h4>
+     <ul class="clean-list flex wrap">
+          <component v-for="note in notes" :key="note.id" :is='note.type' :note="note" style="position:relative; padding:5px; border-radius: 5px; border:0.5px solid gray; margin:1.5%; solid gray; width:30%;" >
+          </component>       
      </ul>
+     </div>
     `,
   components: {
     txtNotePrv,
